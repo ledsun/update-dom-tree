031ed2276a079f174eb98fb5abba8257200f4ab5
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-08-03 16:41:13 +0900

    bugfix: Old attributes of reuse DOM elements rest

--- example.js
+++ example.js
@@ -10,28 +10,56 @@ document.body.innerHTML = `
 </div>
 `
 
+const BEFORE_DATA = [{
+  checked: true,
+  message: 'Hello world!'
+}, {
+  checked: true,
+  message: 'Hello world!'
+}]
+const AFTER_DATA = [{
+  checked: false,
+  message: 'Good evening world!'
+}, {
+  checked: false,
+  message: 'Good night world!'
+}]
+
 // Declare a model.
 class Model extends EventEmitter {
   constructor() {
     super()
-    this._message = 'Hello world!'
+    this._hasAttribute = true
+    this._dataList = BEFORE_DATA
   }
 
-  set message(message) {
-    this._message = message
+  set dataList(message) {
+    this._dataList = message
     this.emit('update')
   }
 
-  get message() {
-    return this._message
+  get dataList() {
+    return this._dataList
+  }
+
+  set hasAttribute(val) {
+    this._hasAttribute = val
+  }
+
+  get hasAttribute() {
+    return this._hasAttribute
   }
 }
 
 // Declare a view.
 const template = Handlebars.compile(`
-<div class="message">
-  <span>{{message}}</span>
-</div>
+<ul class="message"{{#if hasAttribute}} hoge{{/if}}>
+  {{#each dataList}}
+  <li>
+    <input type="checkbox"{{#if checked}} checked="checked"{{/if}}><span>{{message}}</span>
+  </li>
+  {{/each}}
+</ul>
 `)
 
 function render(dom, model) {
@@ -43,11 +71,16 @@ function render(dom, model) {
 const model = new Model()
 const dom = document.body.querySelector('.target')
 model.on('update', () => render(dom, model))
+let count = 0
 
 // Bind the model to an user operation events.
-document.addEventListener('click', ({target}) => {
-  if(target.closest('.message')) {
-    model.message = 'Good night world!'
+document.addEventListener('click', ({
+  target
+}) => {
+  if (target.closest('.message')) {
+    model.hasAttribute = false
+    model.dataList = [AFTER_DATA, BEFORE_DATA][count % 2]
+    count += 1
   }
 })
 
