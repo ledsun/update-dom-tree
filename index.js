523257a03c1f3e9ffa0e4e8586a02721febcca84
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-27 03:00:19 +0900

    refactor: Rename an argument

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const parse5 = require('parse5')
 const diffAndUpdateChildren = require('./lib/diff-and-update-children')
 
-module.exports = function(dom, html) {
-  console.assert(dom, 'dom is a necessary parameter')
+module.exports = function(node, html) {
+  console.assert(node, 'node is a necessary parameter')
 
   const ast = parse5.parseFragment(html.trim())
-  diffAndUpdateChildren(ast, dom)
+  diffAndUpdateChildren(ast, node)
 }
