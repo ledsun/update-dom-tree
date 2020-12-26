04e7ea7045b3acc8fb246b8049f2be14c5a83f19
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-27 02:37:50 +0900

    Use console.assert instead of the Node builtin assert

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const assert = require('assert')
 const parse5 = require('parse5')
 const diffAndUpdateChildren = require('./lib/diff-and-update-children')
 
 module.exports = function(dom, html) {
-  assert(dom, 'dom is a necessary parameter')
+  console.assert(dom, 'dom is a necessary parameter')
 
   const ast = parse5.parseFragment(html.trim())
   diffAndUpdateChildren(ast, dom)
